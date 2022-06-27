9641b618f469fbd676aeb84633cb7b5352a7ad73
Debonair2 <dareblessing12@gmail.com>
2022-06-27 15:35:00 +0100

    first commit

---
+++ script.java
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+
+// Bounce button
+
+$("#bouncebutton").click(function(){
+const element = document.querySelector('.bouncebutton');
+element.classList.add('animated', 'bounce');
+setTimeout(function() {
+element.classList.remove('bounce');
+}, 2000);
+});
+
+
+});
